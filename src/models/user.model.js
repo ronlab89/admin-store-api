a4831cc83974880085b67e597d45edd40b62c0bd
Ronald Labrador <ronlabdev@gmail.com>
2025-02-08 16:36:21 -0500

    user model

---
+++ src/models/user.model.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    lowercase: true,
+    index: { unique: true },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  events_history: {
+    user_created_at: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    user_edited_at: [
+      {
+        date: { type: Date },
+        updating_user: { type: Schema.Types.ObjectId, ref: "User" },
+        default: {},
+        _id: false,
+      },
+    ],
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    next();
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error al codificar la contraseña");
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
